d689ed1a545d4e6aaa7fb7f2fc085abbd41fca7f
Clare Constantine <clare.constantine@gmail.com>
2016-01-31 21:55:50 -0800

    add some sample data

--- db/migrate/20160201032204_add_lineitem_columns.rb
+++ db/migrate/20160201032204_add_lineitem_columns.rb
@@ -1,10 +1,10 @@
 class AddLineitemColumns < ActiveRecord::Migration
   def change
-    add_column :lineitems, :campaign_id, :integer
-    add_column :lineitems, :campaign_name, :string
-    add_column :lineitems, :line_item_name, :string
-    add_column :lineitems, :booked_amount, :decimal
-    add_column :lineitems, :actual_amount, :decimal
-    add_column :lineitems, :adjustments, :decimal
+    add_column :line_items, :campaign_id, :integer
+    add_column :line_items, :campaign_name, :string
+    add_column :line_items, :line_item_name, :string
+    add_column :line_items, :booked_amount, :decimal
+    add_column :line_items, :actual_amount, :decimal
+    add_column :line_items, :adjustments, :decimal
   end
 end
--- test/models/line_item_test.rb
+++ test/models/line_item_test.rb
@@ -4,4 +4,9 @@ class LineItemTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "fixtures loaded correctly" do
+    item = LineItem.find(123)
+    assert item.line_item_name == "Intelligent Rubber Car - 771a"
+  end
 end
