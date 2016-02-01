63debc0bd6e5b842847a5d914968382a01ebd6a9
Clare Constantine <clare.constantine@gmail.com>
2016-01-31 23:52:42 -0800

    lineitem subtotal displays on main page

--- app/helpers/billing_helper.rb
+++ app/helpers/billing_helper.rb
@@ -1,13 +1,35 @@
 module BillingHelper
 
   def line_item_row item
-    row = content_tag(:td, item.line_item_name)
-    row << content_tag(:td, item.campaign_name)
-    row << content_tag(:td, item.booked_amount)
-    row << content_tag(:td, item.actual_amount)
-    row << content_tag(:td, item.adjustments)
+    row = item_name_cell item
+    row << campaign_name_cell(item)
+    row << price_cell(item.booked_amount)
+    row << price_cell(item.actual_amount)
+    row << price_cell(item.adjustments)
+    row << price_cell(subtotal(item), "subtotal")
 
     content_tag :tr, row
+  end
+
+  def item_name_cell item
+    content_tag :td, item.line_item_name, :class => "item_name"
+  end
+
+  def campaign_name_cell item
+    # todo: make this a link to just displaying items for this campaign
+    link = item.campaign_name
+    content_tag :td, link, :class => "campaign_name"
+  end
+
+  def price_cell amount, klass=""
+    klasses = "amount " + klass
+
+    value = number_to_currency amount
+    content_tag :td, value, :class => klasses
+  end
+
+  def subtotal item
+    subt = item.actual_amount + item.adjustments
   end
 
 end
